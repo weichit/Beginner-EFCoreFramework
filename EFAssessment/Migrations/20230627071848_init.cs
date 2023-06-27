500a2e9bbfa26cc52b1701e729cf57ea4ac57e25
tan_wei_chit@artc.a-star.edu.sg <tan_wei_chit@artc.a-star.edu.sg>
2023-06-27 15:19:59 +0800

    migration successful

---
+++ EFAssessment/Migrations/20230627071848_init.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFAssessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Appointment_Db");
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                schema: "Appointment_Db",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DoctorName = table.Column<string>(type: "text", nullable: false),
+                    DoctorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsReversed = table.Column<bool>(type: "boolean", nullable: false),
+                    Cost = table.Column<decimal>(type: "numeric", nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                schema: "Appointment_Db",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientName = table.Column<string>(type: "text", nullable: false),
+                    SlotId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReversedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors",
+                schema: "Appointment_Db");
+
+            migrationBuilder.DropTable(
+                name: "Patients",
+                schema: "Appointment_Db");
+        }
+    }
+}
